80aacdb6f574a0d80207750475fad0e24463b3c3
codecrafterak <ajay2008uk@outlook.com>
2025-12-30 00:34:14 +0530

    Emptybin

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define TRIG_PIN 5
+#define ECHO_PIN 18
+
+long duration;
+float distance;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+}
+
+void loop() {
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duration = pulseIn(ECHO_PIN, HIGH, 30000);
+  distance = duration * 0.034 / 2;
+
+  if (duration == 0) {
+    Serial.println("Out of range");
+  } else {
+    Serial.print("Distance: ");
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+
+  delay(1000);
+}
